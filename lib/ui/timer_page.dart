8c246a331f2bc081befb188b6edbcf4e5ba0e251
DevNweYi <nweyiaung610@gmail.com>
2023-12-25 22:18:47 +0630

    implementing state

--- lib/ui/timer_page.dart
+++ lib/ui/timer_page.dart
@@ -7,38 +7,37 @@ class TimerPage extends StatefulWidget {
   State<TimerPage> createState() => _TimerPageState();
 }
 
+enum ButtonState { initial, play, pause }
+
 class _TimerPageState extends State<TimerPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.orange,
-      appBar: AppBar(title: const Text("Ephemeral Timer"),
+      appBar: AppBar(
+        title: const Text("Ephemeral Timer"),
       ),
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("01:00",style: Theme.of(context).textTheme.displayLarge,),
-            const SizedBox(height: 30,),
+            Text(
+              "01:00",
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 FloatingActionButton(
-                  child: const Icon(Icons.play_arrow),
-                  onPressed: (){
-
-                } ),
+                    child: const Icon(Icons.play_arrow), onPressed: () {}),
                 FloatingActionButton(
-                  child: const Icon(Icons.pause),
-                  onPressed: (){
-
-                } ),
+                    child: const Icon(Icons.pause), onPressed: () {}),
                 FloatingActionButton(
-                  child: const Icon(Icons.refresh),
-                  onPressed: (){
-
-                } ),
+                    child: const Icon(Icons.refresh), onPressed: () {}),
               ],
             )
           ],
