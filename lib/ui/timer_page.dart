e1dd71be6a7795bc2f1841127686cbaa3be85b84
DevNweYi <nweyiaung610@gmail.com>
2023-12-25 21:07:50 +0630

    first commit

---
+++ lib/ui/timer_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class TimerPage extends StatefulWidget {
+  const TimerPage({super.key});
+
+  @override
+  State<TimerPage> createState() => _TimerPageState();
+}
+
+class _TimerPageState extends State<TimerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      appBar: AppBar(title: const Text("Ephemeral Timer"),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("01:00",style: Theme.of(context).textTheme.displayLarge,),
+            const SizedBox(height: 30,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  child: const Icon(Icons.play_arrow),
+                  onPressed: (){
+
+                } ),
+                FloatingActionButton(
+                  child: const Icon(Icons.pause),
+                  onPressed: (){
+
+                } ),
+                FloatingActionButton(
+                  child: const Icon(Icons.refresh),
+                  onPressed: (){
+
+                } ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
